2275a069555eec7f3110755d7d37ee4a333f6368
xalanq <xalanq@gmail.com>
2026-04-01 20:32:09 +0800

    release: 发布 0.28.108

--- src/core/version.ts
+++ src/core/version.ts
@@ -1,5 +1,5 @@
 // Copyright (c) 2026 上海飞函安全科技有限公司 (Shanghai Feihan Security Technology Co., Ltd.)
 // SPDX-License-Identifier: Apache-2.0
 
-export const VERSION = '0.28.107';
-export const USER_AGENT = 'feihan-openapi-sdk-js/0.28.107';
+export const VERSION = '0.28.108';
+export const USER_AGENT = 'feihan-openapi-sdk-js/0.28.108';
